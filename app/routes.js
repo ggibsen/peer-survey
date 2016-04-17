f1fd86f3eab53d4d360e1f8786c5f98149aee4d8
ggibsen <ggibsen@users.noreply.github.com>
2016-04-16 22:29:24 -0700

    create feedEntry schema and model

--- app/routes.js
+++ app/routes.js
@@ -34,3 +34,22 @@ feedSchema.index({link : 1}, {unique:true, sparse:true});
  
 var FeedModel = mongoose.model( 'Feed', feedSchema );
 
+
+var feedEntrySchema = new mongoose.Schema({
+         description: { type: String, trim:true },
+         title: { type: String, trim:true },
+         summary: { type: String, trim:true },
+         entryID: { type: String, trim:true },
+         publishedDate: { type: Date },
+         link: { type: String, trim:true  },
+         feedID: { type: mongoose.Schema.Types.ObjectId },
+         state: { type: String, trim:true, lowercase:true, default: 'new' },
+         created: { type: Date, default: Date.now },
+     },
+     { collection: 'feedEntry' }
+);
+ 
+feedEntrySchema.index({entryID : 1});
+feedEntrySchema.index({feedID : 1});
+ 
+var FeedEntryModel = mongoose.model( 'FeedEntry', feedEntrySchema );
