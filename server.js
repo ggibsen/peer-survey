02cb360043301648dd3ceede9416fde799d3c4b6
ggibsen <ggibsen@users.noreply.github.com>
2016-05-08 22:06:40 -0700

    steps for running

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ npm install
 node server.js
 node setup_tests.js
 jasmine-node create_accounts_error_spec.js
+jasmine-node create_accounts_spec.js
+node write_creds.js
+jasmine-node feed_spec.js
 */
 
 var express = require('express');
